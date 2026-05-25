88eafa30e62a59fb9cc30deb945d578c5f72a70d
Anna <kimannais1711@gmail.com>
2026-05-26 01:07:27 +0900

    pgx

--- go.mod
+++ go.mod
@@ -1,16 +1,18 @@
 module fiber-deploy
 
-go 1.24.6
+go 1.25.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.13
-	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.12.3
+	github.com/jackc/pgx/v5 v5.9.2
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -19,5 +21,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,42 +1,37 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
 		log.Println("env не найден, использую переменные окружения")
 	}
 
-	connString := os.Getenv("DATABASE_URL")
-	if connString == "" {
-		log.Fatal("переменная не задана")
+	config, err := pgxpool.ParseConfig(dbUrl)
+	if err != nil {
+		log.Fatalf("ошибка парсинга конфигурации БД: %v", err)
 	}
 
-	db, err := sql.Open("postgres", connString)
+	db, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
 		log.Fatalf("ошибка подключения к БД: %v", err)
 	}
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("ошибка подключения к БД: %v", err)
-	}
 	log.Println("Подключение установлено!")
 
 	app := fiber.New()
 	app.Get("/test", func(c *fiber.Ctx) error {
 		var now string
-		err := db.QueryRow("SELECT NOW()").Scan(&now)
+		err := db.QueryRow(c.UserContext(), "SELECT NOW()").Scan(&now)
 		if err != nil {
 			return c.Status(500).SendString("Ошибка запроса к БД")
 		}
